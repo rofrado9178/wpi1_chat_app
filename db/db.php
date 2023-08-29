d1e193f70ce06528cd403e77e7bf666da28fea35
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-08-29 12:06:42 -0400

    setup intial db

--- db/db.php
+++ db/db.php
@@ -1,5 +1,17 @@
 <?php
 
+$host = "localhost";
+$user = "user";
+$password = "PleskDB1234";
+$db_name = "wpi1_chat_app";
+
+$db = mysqli_connect($host, $user, $password, $db_name);
+
+$db->set_charset("utf8mb4");
+
+if($db->connect_error){
+  die('Connection Error ('.$db->connect_errno.')'.$db->connect_error);
+}
 
 
 ?>
